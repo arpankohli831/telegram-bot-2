d34b048d19a5404a1c9a2ef30f446f254d2c5c9e
arpankohli831 <arpankohli831@gmail.com>
2026-02-28 01:25:45 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // ===== BOT SETTINGS =====
-export const BOT_TOKEN = "8767042514:AAGqnQSKeH1qHVc2nQqEY3repS6EyvBxCrg";
+export const BOT_TOKEN = process.env.BOT_TOKEN; // SAFE (Railway)
 export const ADMIN_ID = 7853887140; // your Telegram numeric ID
 
 // ===== PAYMENT DETAILS =====
 export const UPI_ID = "7908684711@fam";
-export const QR_PATH = "./qr.png";   // 👈 LOCAL FILE
+export const QR_PATH = "./qr.png";   // local QR file
 
 // ===== REFERRAL =====
 export const REF_BONUS = 0.5;
 
 // ===== CONTACT & CHANNEL =====
-export const CHANNEL_LINK = "https://t.me/+qWBcAAqb33Q3MmE1;
+export const CHANNEL_LINK = "https://t.me/+qWBcAAqb33Q3MmE1";
 export const OWNER_USERNAME = "ARPANMODX"; // without @
